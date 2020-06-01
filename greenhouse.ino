d286cf404ea9e8439afa702507d5d3d11e70da03
Pieter <plewyllie@gmail.com>
2020-06-01 15:29:43 +0200

    no concurrency is no good

--- greenhouse.ino
+++ greenhouse.ino
@@ -1,13 +1,21 @@
-#include "SevSeg.h"
 
-#include <dht_nonblocking.h>
-#define DHT_SENSOR_TYPE DHT_TYPE_11
 
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+
+#include "DHT.h"
+#include "SevSeg.h" // Required for Segments
 #include <SPI.h>
 #include <Ethernet.h>
 
+#define DHTPIN 22     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
 // Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
+// The IP address will be dependent on your local network:dh
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
@@ -18,74 +26,47 @@ IPAddress ip(192, 168, 128, 56);
 // (port 80 is default for HTTP):
 EthernetServer server(80);
 
-static const int DHT_SENSOR_PIN = 52;
-DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 int finalNb = 1111;
-
 SevSeg sevseg; //Initiate a seven segment controller object
 
 void setup() {
-
-  byte numDigits = 4;
-  
-  byte digitPins[] = {2, 3, 4, 5};
-  
-  byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
-  
-  sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
-  
-  sevseg.setBrightness(90);
-
   Serial.begin(9600);
-}
 
-/*
- * Poll for a measurement, keeping the state machine alive.  Returns
- * true if a measurement is available.
- */
-static bool measure_environment( float *temperature, float *humidity )
-{
-  static unsigned long measurement_timestamp = millis( );
+  // Segments setup
+  byte numDigits = 4;
+  byte digitPins[] = {2, 3, 4, 5}; 
+  byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
+  sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
+  sevseg.setBrightness(90);
+  
+  // DHT
+  dht.begin();
 
-  /* Measure once every four seconds. */
-  if( millis( ) - measurement_timestamp > 3000ul )
-  {
-    if( dht_sensor.measure( temperature, humidity ) == true )
-    {
-      measurement_timestamp = millis( );
-      return( true );
-    }
-  }
-
-  return( false );
 }
 
 void loop() {
-  delay(2000);
-  //sevseg.setNumber(finalNb, 2);
-  //sevseg.refreshDisplay(); // Must run repeatedly
-
-  float temperature;
-  float humidity;
+    
+  /*sevseg.setNumber(finalNb, 2);
+  sevseg.refreshDisplay(); // Must run repeatedly
+  
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  Serial.print( "T = " );
+  Serial.print( t, 1 );
+  Serial.print( " deg. C, H = " );
+  Serial.print( h, 1 );
+  Serial.println( "%" );*/
 
   /* Measure temperature and humidity.  If the functions returns
      true, then a measurement is available. */
-  //if( measure_environment( &temperature, &humidity ) == true )
-  //{
-    dht_sensor.measure( &temperature, &humidity );
-    Serial.print( "T = " );
-    Serial.print( temperature, 1 );
-    Serial.print( " deg. C, H = " );
-    Serial.print( humidity, 1 );
  
-
-    int dispTemp = (int) temperature;
-    int dispHumidity = (int) humidity;
-    finalNb = dispTemp;
-    finalNb *= 100;
-    finalNb += dispHumidity;
-    Serial.print(" Final = ");
-    Serial.print(finalNb, 1);
-    Serial.println( "%" );
-  //}
+  /*int dispTemp = (int) t;
+  int dispHumidity = (int) h;
+  finalNb = dispTemp;
+  finalNb *= 100;
+  finalNb += dispHumidity;
+  Serial.print(" Final = ");
+  Serial.print(finalNb, 1);
+  */
 }
