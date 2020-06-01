de2b4640c1721b5efe52bc6768c499e228b547cf
Pieter <plewyllie@gmail.com>
2020-06-01 15:52:56 +0200

    sketch serre works better

--- sketch_serre/sketch_serre.ino
+++ sketch_serre/sketch_serre.ino
@@ -1,27 +1,63 @@
 #include "SevSeg.h"
-
 #include <dht_nonblocking.h>
+#include <SPI.h> // for webserver
+#include <Ethernet.h> // for ethernet
+
 #define DHT_SENSOR_TYPE DHT_TYPE_11
 
+// DHT Init
 static const int DHT_SENSOR_PIN = 22;
 DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 int finalNb = 1111;
 
+// SevSeg Init
 SevSeg sevseg; //Initiate a seven segment controller object
 
+// Ethernet Init
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 128, 56);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetServer server(80);
+
 void setup() {
-
-  byte numDigits = 4;
-  
-  byte digitPins[] = {2, 3, 4, 5};
-  
-  byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
-  
-  sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
-  
-  sevseg.setBrightness(90);
-
   Serial.begin(9600);
+    while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Ethernet WebServer Example");
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+
+  // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+
+  // start the server
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+
+  // SevSeg init
+  byte numDigits = 4;
+  byte digitPins[] = {2, 3, 4, 5};
+  byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
+  sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
+  sevseg.setBrightness(90);
 }
 
 /*
@@ -71,5 +107,53 @@ void loop() {
     Serial.print(" Final = ");
     Serial.print(finalNb, 1);
     Serial.println( "%" );
+
+    // listen for incoming clients
+    EthernetClient client = server.available();
+    if (client) {
+      Serial.println("new client");
+      // an http request ends with a blank line
+      boolean currentLineIsBlank = true;
+      while (client.connected()) {
+        if (client.available()) {
+          char c = client.read();
+          Serial.write(c);
+          // if you've gotten to the end of the line (received a newline
+          // character) and the line is blank, the http request has ended,
+          // so you can send a reply
+          if (c == '\n' && currentLineIsBlank) {
+            // send a standard http response header
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/html");
+            client.println("Connection: close");  // the connection will be closed after completion of the response
+            client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+            client.println();
+            client.println("<!DOCTYPE HTML>");
+            client.println("<html>");
+            // output the value of each analog input pin
+            client.print("Temp ");
+            client.print(temperature);
+            client.println("<br />");
+            client.print("Humidity ");
+            client.print(humidity);
+            client.println("<br />");
+            client.println("</html>");
+            break;
+          }
+          if (c == '\n') {
+            // you're starting a new line
+            currentLineIsBlank = true;
+          } else if (c != '\r') {
+            // you've gotten a character on the current line
+            currentLineIsBlank = false;
+          }
+        }
+      }
+      // give the web browser time to receive the data
+      delay(1);
+      // close the connection:
+      client.stop();
+      Serial.println("client disconnected");
+    } 
   }
 }
